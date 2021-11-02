0bf998c3b054e98ae0c6dc159a2b15f72c9618f9
mahdisharafi <76534154+mahdisharafi@users.noreply.github.com>
2021-11-02 19:35:45 +0330

    Add files via upload

---
+++ vidlyApp-master/vidlyApp-master/src/components/loginForm.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import { login } from "../services/authService";
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+  doSubmit = async () => {
+    // call the server
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem("token", jwt);
+      // this.props.history.push("/");
+      window.location = "/" ;
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Login:</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </>
+    );
+  }
+}
+
+export default LoginForm;
